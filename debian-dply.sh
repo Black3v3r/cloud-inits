b7d7fc4b93026efbc659c3b0d3b5feffede8088a
Black3v3r <black3v3r@gmail.com>
2017-07-07 14:18:13 +0200

    Change zsh theme

--- debian-dply.sh
+++ debian-dply.sh
@@ -6,15 +6,18 @@ pwd
 export HOME="/root"
 echo $HOME
 
+sed -ri 's/(Port 22)/\1\nPort 443/' /etc/ssh/sshd_config
 echo "\nPort 443" >> /etc/ssh/sshd_config
 service ssh restart
 
-apt-get update -y
+apt-get update -y && apt-get upgrade -y
 apt-get install -y vim git curl wget zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 chsh -s /bin/zsh
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="candy"/g' ~/.zshrc
+source ~/.zshrc
 
-echo "syntax on" >> /root/.vimrc
-echo "set nu" >> /root/.vimrc
+echo "syntax on" >> ~/.vimrc
+echo "set nu" >> ~/.vimrc
